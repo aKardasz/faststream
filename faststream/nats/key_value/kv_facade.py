d5545506d02dac8d75d73ba69b6bef12b7fabd52
akardasz <34469992+aKardasz@users.noreply.github.com>
2024-12-13 20:37:05 +0000

    add method to return raw nats KeyValue

--- faststream/nats/key_value/kv_facade.py
+++ faststream/nats/key_value/kv_facade.py
@@ -12,42 +12,42 @@ if TYPE_CHECKING:
 
 class KeyValueFacade:
     def __init__(self, key_value: "KeyValue") -> None:
-        self.key_value = key_value
+        self._key_value = key_value
 
     async def get(self, key: str, revision: Optional[int] = None) -> "KeyValue.Entry":
-        return await self.key_value.get(key=key, revision=revision)
+        return await self._key_value.get(key=key, revision=revision)
 
     async def put(self, key: str, value: "SendableMessage") -> int:
         payload, _ = encode_message(value)
-        return await self.key_value.put(key=key, value=payload)
+        return await self._key_value.put(key=key, value=payload)
 
     async def create(self, key: str, value: "SendableMessage") -> int:
         payload, _ = encode_message(value)
-        return await self.key_value.create(key=key, value=payload)
+        return await self._key_value.create(key=key, value=payload)
 
     async def update(
         self, key: str, value: "SendableMessage", last: Optional[int] = None
     ) -> int:
         payload, _ = encode_message(value)
-        return await self.key_value.update(key=key, value=payload, last=last)
+        return await self._key_value.update(key=key, value=payload, last=last)
 
     async def delete(self, key: str, last: Optional[int] = None) -> bool:
-        return await self.key_value.delete(key=key, last=last)
+        return await self._key_value.delete(key=key, last=last)
 
     async def purge(self, key: str) -> bool:
-        return await self.key_value.purge(key=key)
+        return await self._key_value.purge(key=key)
 
     async def purge_deletes(self, olderthan: int = 30 * 60) -> bool:
-        return await self.key_value.purge_deletes(olderthan=olderthan)
+        return await self._key_value.purge_deletes(olderthan=olderthan)
 
     async def status(self) -> "KeyValue.BucketStatus":
-        return await self.key_value.status
+        return await self._key_value.status
 
     async def watchall(self, **kwargs: Any) -> "KeyValue.KeyWatcher":
-        return await self.key_value.watchall(**kwargs)
+        return await self._key_value.watchall(**kwargs)
 
     async def history(self, key: str) -> list["KeyValue.Entry"]:
-        return await self.key_value.history(key=key)
+        return await self._key_value.history(key=key)
 
     async def watch(
         self,
@@ -58,7 +58,7 @@ class KeyValueFacade:
         meta_only: bool = False,
         inactive_threshold: Optional[float] = None,
     ) -> "KeyValue.KeyWatcher":
-        await self.key_value.watch(
+        await self._key_value.watch(
             keys=keys,
             headers_only=headers_only,
             include_history=include_history,
@@ -66,3 +66,6 @@ class KeyValueFacade:
             meta_only=meta_only,
             inactive_threshold=inactive_threshold,
         )
+
+    def raw_key_value(self) -> "KeyValue":
+        return self._key_value
